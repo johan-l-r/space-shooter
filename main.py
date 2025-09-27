2ea8321eccb0e68027761bb33579e0cc49d9ed33
johan-l-r <188136149+johan-l-r@users.noreply.github.com>
2025-09-27 14:30:16 +0000

    feat(main): add window border movement restrictions
    now the player can not go out of window bounds

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from os import posix_fallocate
+from typing import Concatenate
 import pygame as pg
-from pygame.display import get_surface
+from os.path import join
+from random import randint
 
 # this needs to be on top
 pg.init()
@@ -11,11 +12,14 @@ WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
 running = True
 
 # window
-window = pg.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT ))
+window = pg.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT ), pg.SRCALPHA)
 
 # player 
-player = pg.image.load("./assets/imgs/alien.png").convert_alpha()
-player = pg.transform.scale(player, (150, 150))
+PLAYER_SIZE = 150
+
+player_surface = pg.image.load(join("assets/imgs", "alien.png")).convert_alpha()
+player_surface = pg.transform.scale(player_surface, (PLAYER_SIZE, PLAYER_SIZE))
+player_rect = player_surface.get_frect(center = (100, WINDOW_HEIGHT / 2))
 
 player_y_pos = (WINDOW_HEIGHT - 150) / 2 # set pos to center of y axis 
 player_speed = 5
@@ -40,12 +44,17 @@ while running:
         player_y_direction = 0
       
   # UPDATE GAME
-  player_y_pos += player_speed * player_y_direction
+  player_rect.y += player_speed * player_y_direction
+
+  if player_rect.y  < 0: 
+    player_rect.y = player_rect.y = 0
+  if player_rect.y > WINDOW_HEIGHT - PLAYER_SIZE:
+    player_rect.y = WINDOW_HEIGHT - PLAYER_SIZE
 
   # DRAW GAME
   window.fill((0, 0, 0)) # fill the window with black every frame
 
-  window.blit(player, (50, player_y_pos))
+  window.blit(player_surface, player_rect)
 
   pg.display.update()
 
